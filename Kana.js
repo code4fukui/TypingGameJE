5d53c99c07c4e80f2254b6593f2e4887ee75684e
Taisuke Fukuno <fukuno@jig.jp>
2023-03-10 05:42:26 +0900

    use imports from the base project

--- Kana.js
+++
@@ -1,4 +0,0 @@
-export const Kana = {
-  katakanaToHiragana: s => s.replace(/[\u30a1-\u30f6]/g, m => String.fromCharCode(m.charCodeAt(0) - 0x60)),
-  hiraganaToKatagana: s => s.replace(/[\u3041-\u3096]/g, m => String.fromCharCode(m.charCodeAt(0) + 0x60)),
-};
